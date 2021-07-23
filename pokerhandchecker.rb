c1839c99360cd5721113a0aa23f704db66b3324b
kirillgrinash <70099350+kirillgrinash@users.noreply.github.com>
2021-07-23 01:29:24 -0500

    added final rspec test for pairs_fullhouse && cleaned up simplified errors on pairs_fullhouse && cleaned up code

--- pokerhandchecker.rb
+++ pokerhandchecker.rb
@@ -14,24 +14,24 @@ class Hand
   end
 
   def face_check
-    @card.each { |card|
+    @card.each do |card|
       card = card.chop
       unless card == "A" || card == "J" || card == "Q" || card == "K" || card.to_i.between?(2, 10) == true
         p "Invalid face input #{card}"
         return false
       end
-    }
+    end
     true
   end
 
   def suit_check
-    @card.each { |card|
+    @card.each do |card|
       card = card.slice(-1,1)
       if card != "H" && card != "D" && card != "C" && card != "S"
         p "Invalid suit input #{card}"
         return false
       end
-    }
+    end
     true
   end
 
@@ -52,19 +52,19 @@ class Hand
   end
 
   # Checks for Straight Flush, Flush and Straight
-  # Dirty code, need to create a new approach to catch straights
+  # Should be divided up and simplified
   def straight_flush
     straight = 0
     flush = 0
     face_card_counter = {}
-    @card.each { |card|
+    @card.each do |card|
       card = card.chop
       if face_card_counter[card] == nil
         face_card_counter[card] = 1
       else
         face_card_counter[card] += 1
       end
-    }
+    end
     if face_card_counter["A"] == 1 && face_card_counter["K"] == 1 && face_card_counter["Q"] == 1 && face_card_counter["J"] == 1 && face_card_counter["10"] == 1  
       straight += 1
     elsif
@@ -98,78 +98,74 @@ class Hand
 
     index = 0
     base = @card[0].slice(-1, 1)
-    @card.each { |card|
+    @card.each do |card|
       card = card.slice(-1,1) 
       unless card != base
         index += 1
       end
-    }
+    end
     if index == 5
       flush += 1
     end
 
     if straight + flush == 2
-      p "Straight Flush"
+      return p "Straight Flush"
     elsif
       straight == 1
-      p "Straight"
+      return p "Straight"
     elsif
       flush == 1
-      p "Flush"
+      return p "Flush"
+    else
+      return false
     end
 
   end
 
-  # checks for pairs and fullhouse
+  # Checks for pairs and fullhouse
+  # Should be divided up
   def pairs_fullhouse
     face_card_counter = {}
-    fullhouse_counter = 0
-    @card.each { |card|
+    arr = []
+    @card.each do |card|
       card = card.chop
       if face_card_counter[card] == nil
         face_card_counter[card] = 1
       else
         face_card_counter[card] += 1
       end
-    }
-    arr = []
-    face_card_counter.each { |face, number|
+    end
+    
+    face_card_counter.each do | _ , number|
       if number == 4
-        p "4 of a kind #{face}"
-        arr << 4
+        arr << "a"
       elsif
         number == 3
-        p "3 of a kind #{face}"
-        fullhouse_counter += 1
-        arr << 3
+        arr << "b"
       elsif
         number == 2
-        p "2 of a kind #{face}"
-        fullhouse_counter += 1
-        arr << 2
+        arr << "c"
       end
-    }
-    if fullhouse_counter == 2
-      p "Fullhouse"
-      arr << "F"
     end
-    p arr
-    p face_card_counter
+
+    arr.sort!
+    if arr[0] == "a"
+      return p "Four of a kind"
+    elsif
+      arr[0] == "b" && arr[1] == "c"
+      return p "Full house"
+    elsif
+      arr[0] == "b" 
+      return p "Three of a kind"
+    elsif
+      arr[0] == "c" 
+      return p "Two pair"
+    else
+      false
+    end
   end
 
 end
-
-
-# puts "Hello! Please enter 5 cards. Example: AD 10H 3C KS 4H"
-# puts "First is the card face and last is the card suit"
-# input = gets.chomp
-# poker_hand = Hand.new(input)
-# poker_hand.number_cards
-# poker_hand.face_check
-# poker_hand.suit_check
-# poker_hand.duplicate_check
-# poker_hand.straight_flush
-# poker_hand.pairs_fullhouse
 
 def run
   puts "Hello! Please enter 5 cards. Example: AD 10H 3C KS 4H"
@@ -185,6 +181,9 @@ def run
 end
 
 run
+
+
+
 
 require 'rspec'
 
@@ -258,6 +257,87 @@ RSpec.describe Hand do
     it "should return false, duplicate cards found" do
       poker_hand = Hand.new("AD 10H 10H 2S 4H")
       result = poker_hand.duplicate_check
+      expect(result).to eq(false)
+    end
+  end
+
+  # Test straight_flush triggers straight flush
+  describe "straight_flush" do
+    it "should return straight_flush" do
+      poker_hand = Hand.new("JC 10C 9C 8C 7C")
+      result = poker_hand.straight_flush
+      expect(result).to eq("Straight Flush")
+    end
+  end
+
+  # Test straight_flush triggers flush
+  describe "straight_flush" do
+    it "should return flush" do
+      poker_hand = Hand.new("JD 9D 8D 4D 3D")
+      result = poker_hand.straight_flush
+      expect(result).to eq("Flush")
+    end
+  end
+
+  # Test straight_flush triggers straight
+  describe "straight_flush" do
+    it "should return straight" do
+      poker_hand = Hand.new("10D 9S 8H 7D 6C")
+      result = poker_hand.straight_flush
+      expect(result).to eq("Straight")
+    end
+  end
+
+  # Test straight_flush return false, no trigger
+  describe "straight_flush" do
+    it "should return false, no trigger" do
+      poker_hand = Hand.new("10D 2S 8H AD 6C")
+      result = poker_hand.straight_flush
+      expect(result).to eq(false)
+    end
+  end
+
+  # Test pairs_fullhouse return full house
+  describe "pairs_fullhouse" do
+    it "should return full house" do
+      poker_hand = Hand.new("10D 10S 10H AD AC")
+      result = poker_hand.pairs_fullhouse
+      expect(result).to eq("Full house")
+    end
+  end
+
+  # Test pairs_fullhouse return four of a kind
+  describe "pairs_fullhouse" do
+    it "should return four of a kind" do
+      poker_hand = Hand.new("10D 10S 10H 10C AC")
+      result = poker_hand.pairs_fullhouse
+      expect(result).to eq("Four of a kind")
+    end
+  end
+
+  # Test pairs_fullhouse return three of a kind
+  describe "pairs_fullhouse" do
+    it "should return three of a kind" do
+      poker_hand = Hand.new("10D 10S 10H 5C AC")
+      result = poker_hand.pairs_fullhouse
+      expect(result).to eq("Three of a kind")
+    end
+  end
+
+  # Test pairs_fullhouse return two pair
+  describe "pairs_fullhouse" do
+    it "should return two pair" do
+      poker_hand = Hand.new("10D 10S 6H 5C AC")
+      result = poker_hand.pairs_fullhouse
+      expect(result).to eq("Two pair")
+    end
+  end
+
+  # Test pairs_fullhouse return false
+  describe "pairs_fullhouse" do
+    it "should return false" do
+      poker_hand = Hand.new("10D 6S 8H 5C AC")
+      result = poker_hand.pairs_fullhouse
       expect(result).to eq(false)
     end
   end
